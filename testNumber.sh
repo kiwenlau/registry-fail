089773afd22f22a2cf9be5009ce7d4e15c155427
kiwenlau <kiwenlau@126.com>
2015-07-21 18:36:49 +0900

    all output

--- testNumber.sh
+++ testNumber.sh
@@ -3,10 +3,21 @@
 # 31 latest images for benchmark
 imagelist=(axle-base sultans-bin haproxy cb-shell dnsutils node-metrics container-metrics ruby-base ipsec multilevel drupal jruby openjdk mono glassfish jenkins-slave quickstart-python exhibitor ubuntu-perl swagger-editor serf dnsmasq gocd-base gocd-agent drill ubuntu-perl-dev devmachine buildpack-runner gcc buildstep gocd-server)
 
-# calculate fail rate for each image
-for imagename in ${imagelist[*]};
-do
-	echo -e "$imagename: \c"
-	cat output/$imagename.txt | grep Test | grep -c ""
-done
 
+testNumber()
+{
+	# calculate fail rate for each image
+	for imagename in ${imagelist[*]};
+	do
+		#echo -e "$imagename: \c"
+		cat output/output$1/$imagename.txt | grep Test | grep -c ""
+	done
+}
+
+i=1
+while [ $i -le 12 ]
+do
+	testNumber $i 
+	echo ""
+	((i++))
+done
